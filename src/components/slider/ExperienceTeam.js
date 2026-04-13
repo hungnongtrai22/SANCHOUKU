300680a609eb03eb6684276ded31a091de8df011
VU QUOC HUNG <vuquochung220402@gmail.com>
2026-04-13 09:35:22 +0700

    Update ExperienceTeam.js

--- src/components/slider/ExperienceTeam.js
+++ src/components/slider/ExperienceTeam.js
@@ -1,11 +1,14 @@
-import { Fragment, useRef } from "react";
+import { Fragment, useCallback, useEffect, useRef, useState } from "react";
 import Slider from "react-slick";
 import { useLocales } from "../../locales";
+import axios from "axios";
 
 const ExperienceTeam = () => {
   // Tạo ref để truy cập vào Slider instance
   const sliderRef = useRef(null);
-  const {t} = useLocales();
+  const {t, currentLang} = useLocales();
+    const [employees, setEmployees] = useState([]);
+
 
   const next = () => {
     sliderRef.current.slickNext();
@@ -23,7 +26,7 @@ const ExperienceTeam = () => {
     arrows: false,
     speed: 1000,
     focusOnSelect: false,
-    slidesToShow: 4,
+    slidesToShow: employees?.length > 4 ? 4: employees?.length,
     slidesToScroll: 2,
     responsive: [
       {
@@ -47,6 +50,27 @@ const ExperienceTeam = () => {
       },
     ],
   };
+
+  const getAllEmployeeHandler = useCallback(
+    async () => {
+      // console.log("Month",date.getMonth());
+      // console.log("Year",date.getFullYear());
+      const { data } = await axios.get(
+        `${process.env.NEXT_PUBLIC_BE_HOST}/api/employee/list`
+      );
+            console.log("TEST", data.employees);
+
+      setEmployees(data.employees);
+    },
+    []
+    // [date]
+  );
+
+  useEffect(() => {
+    getAllEmployeeHandler();
+  }, []);
+
+  console.log("Test", employees);
 
   return (
     <Fragment>
@@ -76,9 +100,9 @@ const ExperienceTeam = () => {
         ref={sliderRef} // Gán ref vào đây
         className="team-active"
       >
-        <div className="team-member wow fadeInUp delay-0-2s">
+        {employees?.map((item, index) => <div key={index} className="team-member wow fadeInUp delay-0-2s">
           <div className="image">
-            <img src="assets/images/team/member1.jpg" alt="Team Member" />
+            <img src={item.image[0].url} alt="Team Member" style={{width: "220px", height: "220px"}}/>
             <div className="social-style-one">
               <a href="#">
                 <i className="fab fa-facebook-f" />
@@ -94,196 +118,15 @@ const ExperienceTeam = () => {
               </a>
             </div>
           </div>
-          <h4>Joseph C. Roberson</h4>
-          <span className="designation">Senior Farmer</span>
+          <h4>{currentLang.value === 'jp'? item.nameJP : item.name}</h4>
+          <span className="designation">{currentLang.value === 'jp'? item.positionJP : item.position}</span>
           <img
             className="team-bg"
             src="assets/images/team/team-member-bg.png"
             alt="Bg"
           />
-        </div>
-        <div className="team-member wow fadeInUp delay-0-3s">
-          <div className="image">
-            <img src="assets/images/team/member2.jpg" alt="Team Member" />
-            <div className="social-style-one">
-              <a href="#">
-                <i className="fab fa-facebook-f" />
-              </a>
-              <a href="#">
-                <i className="fab fa-twitter" />
-              </a>
-              <a href="#">
-                <i className="fab fa-youtube" />
-              </a>
-              <a href="#">
-                <i className="fab fa-instagram" />
-              </a>
-            </div>
-          </div>
-          <h4>Timothy N. Allison</h4>
-          <span className="designation">Junior Manager</span>
-          <img
-            className="team-bg"
-            src="assets/images/team/team-member-bg.png"
-            alt="Bg"
-          />
-        </div>
-        <div className="team-member wow fadeInUp delay-0-4s">
-          <div className="image">
-            <img src="assets/images/team/member3.jpg" alt="Team Member" />
-            <div className="social-style-one">
-              <a href="#">
-                <i className="fab fa-facebook-f" />
-              </a>
-              <a href="#">
-                <i className="fab fa-twitter" />
-              </a>
-              <a href="#">
-                <i className="fab fa-youtube" />
-              </a>
-              <a href="#">
-                <i className="fab fa-instagram" />
-              </a>
-            </div>
-          </div>
-          <h4>Richard L. McDonald</h4>
-          <span className="designation">Business Consultant</span>
-          <img
-            className="team-bg"
-            src="assets/images/team/team-member-bg.png"
-            alt="Bg"
-          />
-        </div>
-        <div className="team-member wow fadeInUp delay-0-5s">
-          <div className="image">
-            <img src="assets/images/team/member4.jpg" alt="Team Member" />
-            <div className="social-style-one">
-              <a href="#">
-                <i className="fab fa-facebook-f" />
-              </a>
-              <a href="#">
-                <i className="fab fa-twitter" />
-              </a>
-              <a href="#">
-                <i className="fab fa-youtube" />
-              </a>
-              <a href="#">
-                <i className="fab fa-instagram" />
-              </a>
-            </div>
-          </div>
-          <h4>Gregory J. Vansant</h4>
-          <span className="designation">CO- Founder</span>
-          <img
-            className="team-bg"
-            src="assets/images/team/team-member-bg.png"
-            alt="Bg"
-          />
-        </div>
-        <div className="team-member wow fadeInUp delay-0-6s">
-          <div className="image">
-            <img src="assets/images/team/member1.jpg" alt="Team Member" />
-            <div className="social-style-one">
-              <a href="#">
-                <i className="fab fa-facebook-f" />
-              </a>
-              <a href="#">
-                <i className="fab fa-twitter" />
-              </a>
-              <a href="#">
-                <i className="fab fa-youtube" />
-              </a>
-              <a href="#">
-                <i className="fab fa-instagram" />
-              </a>
-            </div>
-          </div>
-          <h4>Joseph C. Roberson</h4>
-          <span className="designation">Senior Farmer</span>
-          <img
-            className="team-bg"
-            src="assets/images/team/team-member-bg.png"
-            alt="Bg"
-          />
-        </div>
-        <div className="team-member wow fadeInUp delay-0-7s">
-          <div className="image">
-            <img src="assets/images/team/member2.jpg" alt="Team Member" />
-            <div className="social-style-one">
-              <a href="#">
-                <i className="fab fa-facebook-f" />
-              </a>
-              <a href="#">
-                <i className="fab fa-twitter" />
-              </a>
-              <a href="#">
-                <i className="fab fa-youtube" />
-              </a>
-              <a href="#">
-                <i className="fab fa-instagram" />
-              </a>
-            </div>
-          </div>
-          <h4>Timothy N. Allison</h4>
-          <span className="designation">Junior Manager</span>
-          <img
-            className="team-bg"
-            src="assets/images/team/team-member-bg.png"
-            alt="Bg"
-          />
-        </div>
-        <div className="team-member wow fadeInUp delay-0-8s">
-          <div className="image">
-            <img src="assets/images/team/member3.jpg" alt="Team Member" />
-            <div className="social-style-one">
-              <a href="#">
-                <i className="fab fa-facebook-f" />
-              </a>
-              <a href="#">
-                <i className="fab fa-twitter" />
-              </a>
-              <a href="#">
-                <i className="fab fa-youtube" />
-              </a>
-              <a href="#">
-                <i className="fab fa-instagram" />
-              </a>
-            </div>
-          </div>
-          <h4>Richard L. McDonald</h4>
-          <span className="designation">Business Consultant</span>
-          <img
-            className="team-bg"
-            src="assets/images/team/team-member-bg.png"
-            alt="Bg"
-          />
-        </div>
-        <div className="team-member wow fadeInUp delay-0-9s">
-          <div className="image">
-            <img src="assets/images/team/member4.jpg" alt="Team Member" />
-            <div className="social-style-one">
-              <a href="#">
-                <i className="fab fa-facebook-f" />
-              </a>
-              <a href="#">
-                <i className="fab fa-twitter" />
-              </a>
-              <a href="#">
-                <i className="fab fa-youtube" />
-              </a>
-              <a href="#">
-                <i className="fab fa-instagram" />
-              </a>
-            </div>
-          </div>
-          <h4>Gregory J. Vansant</h4>
-          <span className="designation">CO- Founder</span>
-          <img
-            className="team-bg"
-            src="assets/images/team/team-member-bg.png"
-            alt="Bg"
-          />
-        </div>
+        </div>)}
+      
       </Slider>
     </Fragment>
   );
